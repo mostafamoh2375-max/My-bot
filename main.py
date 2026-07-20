700d387f923e8422f82d5edff4d4d45a64799946
mostafamoh2375-max <mostafamoh2375@gmail.com>
2026-07-20 08:16:04 +0300

    Add Flask web server to main.py

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import json
 import uuid
 import time
 import logging
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
 
 import telebot
 from telebot import types
